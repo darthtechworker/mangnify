7b0e5e5d3ee5ae00d6a6070cdd7bb19e0e563ead
darthtechworker <gaurav91pandey@gmail.com>
2024-11-30 01:07:47 -0800

    updated app title and size

--- src/mangnify/app.py
+++ src/mangnify/app.py
@@ -1,23 +1,18 @@
-"""
-My first application
-"""
-
 import toga
-from toga.style import Pack
-from toga.style.pack import COLUMN, ROW
 
 
 class Mangnify(toga.App):
     def startup(self):
-        """Construct and show the Toga application.
-
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
+        """
+        App startup
         """
         main_box = toga.Box()
 
-        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.main_window = toga.MainWindow(
+            title=f"{self.formal_name} v{self.version}",
+            size=(400, 400),
+            resizeable=False,
+        )
         self.main_window.content = main_box
         self.main_window.show()
 
