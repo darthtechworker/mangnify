4c0ae03c0d57cc1a4819fc65d5dc91bad56bd1ac
darthtechworker <gaurav91pandey@gmail.com>
2024-11-30 21:52:51 -0800

    additional logging

--- src/mangnify/ui/components/execution_buttons_container.py
+++ src/mangnify/ui/components/execution_buttons_container.py
@@ -1,8 +1,12 @@
 import toga
 from toga.style.pack import ROW, Pack
 
+from mangnify.utils import logging
+
 START_BUTTON_LABEL = "Start"
 ABORT_BUTTON_LABEL = "Abort"
+
+logger = logging.getLogger(__name__)
 
 
 def build_execution_buttons_container(app):
@@ -33,8 +37,21 @@ def on_press_start_button(widget, app):
     """
     Handle the press event on the start button.
     """
-
-    pass
+    logger.info("Options Selected:")
+    logger.info(f"Input Directory: {app.input_directory}")
+    logger.info(f"Output Directory: {app.output_directory}")
+    logger.info(f"Working Directory: {app.working_directory}")
+    logger.info(f"Is Processing Needed: {app.is_processing_needed}")
+    logger.info(f"Is Keep Images: {app.is_keep_images}")
+    logger.info(f"JPG Quality: {app.jpg_quality}")
+    logger.info(f"Is Trim Images: {app.is_trim_images}")
+    logger.info(f"Trim Limit: {app.trim_limit}")
+    logger.info(f"Add Margins: {app.add_margins}")
+    logger.info(f"Is Rotate Spread: {app.is_rotate_spread}")
+    logger.info(f"Is CBZ Needed: {app.is_cbz_needed}")
+    logger.info(f"Is AZW3 Needed: {app.is_azw3_needed}")
+    logger.info(f"Is Grayscale: {app.is_grayscale}")
+    logger.info(f"Compression Level: {app.compression_level}")
 
 
 def on_press_abort_button(widget, app):
