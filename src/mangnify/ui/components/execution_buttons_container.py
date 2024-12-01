4fe8f14fe5f52163c7cb440f8162e0dc6a55d7f4
darthtechworker <gaurav91pandey@gmail.com>
2024-11-30 19:39:29 -0800

    added execution buttons

---
+++ src/mangnify/ui/components/execution_buttons_container.py
@@ -0,0 +1,45 @@
+import toga
+from toga.style.pack import Pack, ROW
+
+START_BUTTON_LABEL = "Start"
+ABORT_BUTTON_LABEL = "Abort"
+
+
+def build_execution_buttons_container(app):
+    """
+    Build the execution buttons container.
+    """
+
+    app.start_button = toga.Button(
+        text=START_BUTTON_LABEL,
+        on_press=lambda widget: on_press_start_button(widget, app),
+        style=Pack(flex=1, height=30, padding=(0, 20), background_color="#0093E6"),
+    )
+
+    app.abort_button = toga.Button(
+        text=ABORT_BUTTON_LABEL,
+        on_press=lambda widget: on_press_abort_button(widget, app),
+        style=Pack(flex=1, height=30, padding=(0, 20), background_color="#E65300"),
+    )
+
+    app.execution_buttons_container = toga.Box(
+        style=Pack(direction=ROW, padding=(7, 40, 10, 40))
+    )
+    app.execution_buttons_container.add(app.start_button)
+    app.execution_buttons_container.add(app.abort_button)
+
+
+def on_press_start_button(widget, app):
+    """
+    Handle the press event on the start button.
+    """
+
+    pass
+
+
+def on_press_abort_button(widget, app):
+    """
+    Handle the press event on the abort button.
+    """
+
+    pass
--- src/mangnify/ui/main.py
+++ src/mangnify/ui/main.py
@@ -2,6 +2,9 @@ from mangnify.ui.components.azw3_options_container import build_azw3_options_con
 from mangnify.ui.components.comic_info_options_container import (
     build_comic_info_options_container,
 )
+from mangnify.ui.components.execution_buttons_container import (
+    build_execution_buttons_container,
+)
 from mangnify.ui.components.image_options_container import build_image_options_container
 from mangnify.ui.components.select_input_directory import (
     build_select_input_directory_button,
@@ -15,7 +18,7 @@ from mangnify.ui.components.dynamic_container import build_dynamic_container
 
 def build_ui(app):
     """
-    Initialize the UI.
+    Build the UI.
     """
 
     build_select_input_directory_button(app)
@@ -24,10 +27,20 @@ def build_ui(app):
     build_image_options_container(app)
     build_comic_info_options_container(app)
     build_azw3_options_container(app)
+    build_execution_buttons_container(app)
 
     app.main_box.add(app.select_input_directory_button)
     app.main_box.add(app.select_options_dropdown)
     app.main_box.add(app.dynamic_container)
+    app.main_box.add(app.execution_buttons_container)
+
+    init_ui(app)
+
+
+def init_ui(app):
+    """
+    Initialize the UI.
+    """
 
     app.select_options_dropdown.value = OPTIONS[0]
 
@@ -38,3 +51,5 @@ def build_ui(app):
     app.rotate_spread_checkbox.value = False
 
     # TODO: read and update comic info options from comic info file here
+
+    app.abort_button.enabled = False
