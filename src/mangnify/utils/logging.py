c7cc0283614738a2d437f200bd90f148137f21e8
darthtechworker <gaurav91pandey@gmail.com>
2024-12-01 18:29:20 -0800

    updated logging to include file name

--- src/mangnify/utils/logging.py
+++ src/mangnify/utils/logging.py
@@ -7,7 +7,7 @@ def getLogger(name):
     """
 
     logging.basicConfig(
-        level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s"
+        level=logging.INFO, format="%(asctime)s %(filename)s %(levelname)s %(message)s"
     )
 
     return logging.getLogger(name)
