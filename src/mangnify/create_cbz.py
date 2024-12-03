2699cb6435265612528ad029ffbdabc049b8277c
darthtechworker <gaurav91pandey@gmail.com>
2024-12-02 20:19:57 -0800

    added cbz creation functionality

--- src/mangnify/create_cbz.py
+++ src/mangnify/create_cbz.py
@@ -1,4 +1,6 @@
 from mangnify.utils import logging
+from mangnify.utils.comic_info import write_comic_info
+from mangnify.utils.file import copy_images, create_directory, package_cbz
 from mangnify.utils.ui import update_log_area_callback
 
 logger = logging.getLogger(__name__)
@@ -11,5 +13,21 @@ def create_cbz(app) -> None:
 
     logger.info("Creating CBZ file...")
     update_log_area_callback(app, "\nCreating CBZ file...")
+
+    try:
+        create_directory(app.output_directory)
+        create_directory(app.working_directory)
+
+        if not app.is_processing_needed:
+            copy_images(app.input_directory, app.working_directory)
+
+        write_comic_info(app)
+
+        package_cbz(app.working_directory, app.output_directory, app.title_input.value)
+
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
     logger.info("CBZ file created.")
     update_log_area_callback(app, "CBZ file created.")
--- src/mangnify/utils/file.py
+++ src/mangnify/utils/file.py
@@ -1,8 +1,23 @@
 import os
+import re
 import shutil
+import unicodedata
+import zipfile
 from typing import List
 
 SUPPORTED_IMAGE_EXTENSIONS = [".jpg", ".jpeg", ".png"]
+
+
+def cleanup(directory_path) -> None:
+    """
+    Cleanup the working directory.
+
+    Parameters:
+    directory_path (str): The path of the directory to cleanup
+    """
+
+    if os.path.exists(directory_path):
+        shutil.rmtree(directory_path)
 
 
 def create_directory(directory_path: str) -> None:
@@ -52,3 +67,78 @@ def copy_images(input_directory, output_directory) -> None:
             image_path = os.path.join(input_directory, image_name)
             output_path = os.path.join(output_directory, image_name)
             shutil.copy(image_path, output_path)
+
+
+def sanitize_title(title: str, max_length: int = 100) -> str:
+    """
+    Sanitizes a title for use as a filename.
+
+    Parameters:
+    title (str): The title to sanitize.
+
+    Returns:
+    str: A sanitized filename-safe title.
+    """
+
+    title = unicodedata.normalize("NFC", title)
+
+    title = re.sub(r'[<>:"/\\|?*\n\r\t]', "_", title)
+
+    title = "".join(char for char in title if char.isprintable())
+
+    title = title.strip(" .")
+
+    if len(title) > max_length:
+        title = title[:max_length].rstrip(" .")
+
+    reserved_names = {
+        "CON",
+        "PRN",
+        "AUX",
+        "NUL",
+        "COM1",
+        "COM2",
+        "COM3",
+        "COM4",
+        "COM5",
+        "COM6",
+        "COM7",
+        "COM8",
+        "COM9",
+        "LPT1",
+        "LPT2",
+        "LPT3",
+        "LPT4",
+        "LPT5",
+        "LPT6",
+        "LPT7",
+        "LPT8",
+        "LPT9",
+    }
+    if title.upper() in reserved_names:
+        title += "_file"
+
+    return title
+
+
+def package_cbz(input_directory, output_directory, title) -> None:
+    """
+    Package images into a CBZ file.
+
+    Parameters:
+    input_directory (str): The path of the input directory.
+    output_directory (str): The path of the output directory.
+    """
+
+    santized_title = sanitize_title(title)
+    cbz_path = os.path.join(output_directory, f"{santized_title}.cbz")
+
+    with zipfile.ZipFile(cbz_path, "w") as cbz_file:
+        for image_name in os.listdir(input_directory):
+            if image_name.lower().endswith(tuple(SUPPORTED_IMAGE_EXTENSIONS)):
+                image_path = os.path.join(input_directory, image_name)
+                cbz_file.write(image_path, arcname=image_name)
+
+        comic_info_path = os.path.join(input_directory, "ComicInfo.xml")
+        if os.path.exists(comic_info_path):
+            cbz_file.write(comic_info_path, arcname="ComicInfo.xml")
